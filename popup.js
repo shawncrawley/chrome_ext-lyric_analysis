517d16148ab704879128f9d43a10beed84f4a94f
Shawn Crawley <shawncrawley@gmail.com>
2016-03-29 23:21:26 -0600

    Added custom icons; changed app name; minor modifications to code

--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {'songsToDisable': songsToDisable});
         });
+        $(this).prop('disabled', true);
     });
 
     $('#btn-select-all').on('click', function () {
